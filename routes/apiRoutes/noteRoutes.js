6ed515f91939e993c6f93f8d662ba9811cee59df
Joshua D Wood <joshuawd190@gmail.com>
2021-08-27 06:09:43 -0500

    Attempts at delete, bugfixes

--- routes/apiRoutes/noteRoutes.js
+++ routes/apiRoutes/noteRoutes.js
@@ -3,19 +3,20 @@ const notes = require('../../db/db.json');
 const { createID, createNote, deleteNote } = require('../../lib/noteMethods');
 
 router.get('/notes', (req, res) => {
-  let results = notes;
-  res.json(results);
+  res.json(notes);
 });
 
 router.post('/notes', (req, res) => {
   req.body.id = createID();
-  const note = createNote(req.body, notes);
+  let note = createNote(req.body, notes);
 
   res.json(note);
 });
 
 router.delete('/notes/:id', (req, res) => {
-  const deletedNote = deleteNote(req.params.id, notes);
+  deleteNote(req.params.id, notes);
 
-  res.json(deletedNote);
+  res.status(202);
 });
+
+module.exports = router;
